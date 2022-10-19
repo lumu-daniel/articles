ae1545aed844f3704a7e07c3d6fbef9471c0780e
joNA <jitzian@gmail.com>
2022-10-19 16:44:37 -0400

    Updating TODO: Documentation

--- app/src/main/java/org/com/testing/with/simpletest/RVCustomAdapter.kt
+++ app/src/main/java/org/com/testing/with/simpletest/RVCustomAdapter.kt
@@ -1,7 +1,10 @@
 package org.com.testing.with.simpletest
 
+/**
+ * TODO: Implement the Adapter that will populate a RecyclerView using the information generated in ViewModel
+ * */
+
 class RVCustomAdapter {
-    //TODO: Implement the Adapter()
 
 
 }
